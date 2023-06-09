7c10abac0e9dc2f55c0a931e7fd9c1e5761f71ff
yahuarlocro <yahuarlocro@tutanota.com>
2023-06-10 00:00:01 +0200

    bugfix config variables database connection

--- hotspots-api/src/hotspots_api/config.py
+++ hotspots-api/src/hotspots_api/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     postgres_user: str  
     postgres_pw: str  
     postgres_host: str  
+    postgres_port: int 
     postgres_db: str  
     redis_host: str
     redis_port: int
--- hotspots-api/src/hotspots_api/database.py
+++ hotspots-api/src/hotspots_api/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@127.0.0.1/hotspots"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.postgres_user}:{settings.postgres_pw}@{settings.postgres_host}/{settings.postgres_db}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.postgres_user}:{settings.postgres_pw}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
